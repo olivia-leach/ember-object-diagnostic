db82de65a2f62d74e9e34e710cbd1e4751d1dce0
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-01 09:25:51 -0400

    Respond to ember object diagnostic
    Comfort: 4,
    Clarity: 4
    
    I used the class notes from yesterday & some googling

--- public/diagnostic.js
+++ public/diagnostic.js
@@ -23,3 +23,45 @@
 //  -  Order 1 : 2 hats ($5 each)
 //  -  Order 2 : 1 desk lamp ($20 each)
 //  -  Order 3 : 3 hand towels ($8 each)
+
+const Order = Ember.Object.extend({
+  init: function(){
+    let unitPrice = this.get('unitPrice');
+    let quantity = this.get('quantity');
+  },
+  orderPrice: Ember.computed('unitPrice', 'quantity', function(){
+    return (this.get('unitPrice') * this.get('quantity'));
+  })
+});
+
+const Cart = Ember.Object.extend({
+  orders: [],
+  addToCart: function(item) {
+    let orders = this.get('orders');
+    orders.pushObject(item);
+  },
+  totalPrice: Ember.computed('orders.@each.unitPrice', function(){
+    let orders = this.get('orders');
+    return orders.reduce(function(previousValue, order){
+      return previousValue + order.get('orderPrice');
+    }, 0);
+  })
+});
+
+let myCart = Cart.create({
+});
+
+myCart.addToCart(Order.create({
+  unitPrice: 5,
+  quantity: 2
+}));
+
+myCart.addToCart(Order.create({
+  unitPrice: 20,
+  quantity: 1
+}));
+
+myCart.addToCart(Order.create({
+  unitPrice: 8,
+  quantity: 3
+}));
